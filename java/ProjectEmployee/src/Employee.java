7e5ac8e2ae2c4ebe8925a2abc4aaedf3881be97b
LikhithaKoppula <likhitha.k@comakeit.com>
2022-03-03 12:11:53 +0530

    Renamed folder

---
+++ java/ProjectEmployee/src/Employee.java
@@ -0,0 +1,11 @@
+
+public class Employee {
+ int x; // added attributes
+ 
+ public static void main(String[] args)
+ {
+	 Employee obj = new Employee();
+	 obj.x= 50;
+	 System.out.println(obj.x);
+ }
+}
