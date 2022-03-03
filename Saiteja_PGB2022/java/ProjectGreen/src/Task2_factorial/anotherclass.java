25fe89ace8c0cd8291261c79a7d9d8500814a373
Localadmin <saiteja.b@comakeit.com>
2022-03-03 15:28:49 +0530

    Day2 assignment

---
+++ Saiteja_PGB2022/java/ProjectGreen/src/Task2_factorial/anotherclass.java
@@ -0,0 +1,9 @@
+package Task2_factorial;
+
+public class anotherclass {
+	public static void main(String args[])
+	{
+		System.out.println(factorialcheck.factrecursion(8));
+		System.out.println(factorialcheck.factnonrecursion(8));
+	}
+}
