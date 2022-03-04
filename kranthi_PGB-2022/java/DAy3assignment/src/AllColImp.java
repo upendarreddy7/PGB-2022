04620b7b31025c969aff2ae18268e45463a1d449
Kranthikumarjogu <100823646+Kranthikumarjogu@users.noreply.github.com>
2022-03-04 16:37:00 +0530

    Add files via upload

--- kranthi_PGB-2022/java/DAy3assignment/src/AllColImp.java
+++ kranthi_PGB-2022/java/DAy3assignment/src/AllColImp.java
@@ -1,32 +1,4 @@
 import java.util.*;
- class student {
-	private int rno,age;
-	private String name; private String gender;
-	private String lastname;
-
-	//constructor without arguments
-	 student(){
-
-	}
-
-	// constructer with arguments
-	public student(int rno,int age,String name, String gender,String lastname){
-	this.rno=rno;
-	this.age=age;
-	this.name=name;
-	this.gender=gender;
-	this.lastname=lastname;
-	}
-	public void setName(String Name){this.name=Name;}
-	public String getName() {return name;}
-	public void setRno(int Rno){this.rno=Rno;}
-	public int getRno() {return rno;}
-	public void setAge(int Age){this.age=Age;}
-	public int getAge() {return age;}
-	public void setGender(String Gender){this.gender=Gender;}
-	public String getGender() {return gender;}
-    public void setLastname(String Lastname){this.lastname=Lastname;}
-    public String getLastname() {return lastname;}}
 public class AllColImp {
 	public static void main(String[] args)
     {
