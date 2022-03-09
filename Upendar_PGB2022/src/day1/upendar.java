7e0a15741b1ced7149c5028b5c7c2d639cba6ced
upendarreddy7 <100755096+upendarreddy7@users.noreply.github.com>
2022-03-09 16:06:56 +0530

    Delete Upendar_PGB2022 directory

--- Upendar_PGB2022/src/day1/upendar.java
+++
@@ -1,7 +0,0 @@
-package day1;
-
-public class upendar {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
--- Upendar_PGB2022/src/day2/util/call_Factorial.java
+++
@@ -1,8 +0,0 @@
-package day2.util;
-
-public class call_Factorial {
-    public static void main(String []args) {
-    	System.out.println("Factorial with recursion : "+factorial.with_recursion(6));
-    	System.out.println("Factorial without recursion : "+factorial.without_recursion(6));
-    }
-}
